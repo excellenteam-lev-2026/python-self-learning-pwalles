324ae8a6d30fe518d7f20e6a6f0681476c2ada94
pwalles <pwalles@g.jct.ac.il>
2026-03-25 02:05:03 +0200

    5.1 thats_the_way

---
+++ 5.1.no_vinnigrete.py
@@ -0,0 +1,23 @@
+import datetime
+import random
+
+def no_vinnigrete(date1, date2):
+    timeHelper=date2-date1
+    seconds=timeHelper.total_seconds()
+    seconds=int(seconds)
+    random_seconds=random.randint(0,seconds)
+    delta=datetime.timedelta(seconds=random_seconds)
+    date3 = date1+delta
+    return date3
+
+if __name__ == "__main__":
+    print("Enter the first date (YYYY-MM-DD):")
+    date1_str = input()
+    print("Enter the second date (YYYY-MM-DD) later from the first:")
+    date2_str = input()
+    date1 = datetime.datetime.strptime(date1_str, "%Y-%m-%d")
+    date2 = datetime.datetime.strptime(date2_str, "%Y-%m-%d")
+    date3 = no_vinnigrete(date1, date2)
+    print("The date is:", date3.strftime("%Y-%m-%d"))
+    if date3.isoweekday()==1:
+        print("אין לי ויניגרט")
